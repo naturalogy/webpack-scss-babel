f1186c2f571a9791fb13193ecec14dd1ff6af4ec
S. Skmt <naturalogy@gmail.com>
2019-04-06 15:08:39 +0900

    Remove cache / modify .gitignore

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import 'bootstrap'
-import './sass/main.scss'
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
